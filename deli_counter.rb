cac89a04990f6686c4393e6d2ff265c22058ef43
Tim Donoghue <tmd112593@gmail.com>
2020-02-22 20:59:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+def line(array)
+  result = "The line is currently: "
+  if array.empty? 
+    puts "The line is currently empty."
+  else 
+    array.each_with_index do |element, index|
+      result << (index + 1).to_s << ". " << element << " " 
+    end
+    result.delete_suffix!(" ")
+    puts result 
+  end 
+end 
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end 
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array[0]}."
+    array.delete(array[0])
+  end 
+end 
+    
