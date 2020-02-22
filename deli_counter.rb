9f935b2c2ffebb4b2914e6bc0ea6a666b8ad5f1e
Tim Donoghue <tmd112593@gmail.com>
2020-02-22 20:06:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,9 @@
 katz_deli[] 
 def line(array)
-  result = 
+  result = "The line is currently: "
   if array.empty? 
     puts "The line is currently empty."
   else 
     array.each_with_index do |element, index|
+      result
     
