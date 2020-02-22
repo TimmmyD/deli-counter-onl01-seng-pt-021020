4aa9ca373149e4c3a3208b2225cfc1bfd3c06cda
Tim Donoghue <tmd112593@gmail.com>
2020-02-22 20:45:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(array)
   result = "The line is currently: "
   if array.empty? 
-    puts "The line is currently empty."
+    "The line is currently empty."
   else 
     array.each_with_index do |element, index|
       result << index + 1 << ". " << element
